99e636bc54eb6bda98189869f568df2788cf5c7b
Ethan Blanch <ac111532@avcol.school.nz>
2022-03-23 11:44:36 +1300

    Add project files.

---
+++ BeatLeader/Models/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BeatLeader.Models
+{
+    public class Player
+    {
+        public int PlayerID { get; set; }
+        public char PlayerName { get; set; }
+        public int AccountAge { get; set; }
+        public char PlayerHMD { get; set; }
+        public char PlayerPlatform { get; set; }
+        public int Modcount { get; set; }
+        public int LevelsBeaten { get; set; }
+    }
+}
